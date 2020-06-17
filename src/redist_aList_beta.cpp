83d02cc3a483a07cef904fc78fd294875a5cc008
reiy24 <64212520+reiy24@users.noreply.github.com>
2020-06-17 20:35:31 +0900

    Create redist_aList_beta.cpp

---
+++ src/redist_aList_beta.cpp
@@ -0,0 +1,19 @@
+/////////////////////////////////////
+// Author: Rei Yatsuhashi
+// Institution: American School In Japan
+// Date Created: 2020/06/17
+// Date Modified: 2020/06/17
+// Purpose: Class for Constraints in Redistricting Adjacency
+/////////////////////////////////////
+
+// Header files
+#include <RcppArmadillo.h>
+#include <redist_aList.cpp>
+
+using namespace Rcpp;
+
+// Class to consolidate methods relating to the constraints and tempering for redistricting
+
+class redist_aList_beta: public redist_aList {
+
+}
